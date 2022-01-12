03ba6383981e4aea26999627a66ef92bafacb73a
Yashdhamdhere <yash140699@gmail.com>
2022-01-12 14:00:01 +0530

    first commit

---
+++ src/main/java/com/cg/healthcare/HealthCareSystemApplication.java
@@ -0,0 +1,14 @@
+package com.cg.healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareSystemApplication.class, args);
+		System.out.println(" SPRING BOOT STARTED.........");
+	}
+
+}
